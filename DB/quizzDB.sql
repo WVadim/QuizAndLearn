4e6419710e9d31adda6d987ad50852122cad1f66
JacoboGJ <jacobogj@gmail.com>
2017-04-04 12:25:37 +0200

    MySQL Database
    Comments and some normalization added

--- DB/quizzDB.sql
+++ DB/quizzDB.sql
@@ -2,53 +2,65 @@ CREATE DATABASE quizz;
 
 CREATE TABLE questions ( 
 	questionID		INT UNSIGNED 	NOT NULL AUTO_INCREMENT,
-    questionNearID  INT UNSIGNED    NOT NULL DEFAULT 0,
-	question		VARCHAR(1000)	NOT NULL DEFAULT '',
+    questionNearID  INT UNSIGNED    NOT NULL DEFAULT 0,				#Questions with the same meaning or close to each other, will have the same questionNearID
+	question		VARCHAR(1000)	NOT NULL DEFAULT '',			
 	answer			VARCHAR(500)	NOT NULL DEFAULT '',
-    knowledge		VARCHAR(20)		NOT NULL DEFAULT '',
-	theme			INT UNSIGNED	NOT NULL DEFAULT 0,
-    questionMaker	INT UNSIGNED	NOT NULL DEFAULT '',
+    difficulty		INT UNSIGNED	NOT NULL DEFAULT 0,				
+	theme			INT UNSIGNED	NOT NULL DEFAULT 0,				
+    questionMaker	INT UNSIGNED	NOT NULL DEFAULT '',			
 	PRIMARY KEY (questionID),
-    FOREIGN KEY (questionMaker) REFERENCES users (userID),
-    FOREIGN KEY (theme) REFERENCES theme (ID)
-	);
+    FOREIGN KEY (questionMaker) REFERENCES users (ID),
+    FOREIGN KEY (theme) REFERENCES theme (ID),
+    FOREIGN KEY (difficulty) REFERENCES difficulty (ID)
+);
     
 CREATE TABLE answers (
-	ID				INT UNSIGNED	NOT NULL AUTO_INCREMENT,
+	ID				INT UNSIGNED	NOT NULL AUTO_INCREMENT,		#Storage of most frequent answer (taken as the correct one)
     question		INT UNSIGNED	NOT NULL DEFAULT 0,
     answer			VARCHAR(500)	NOT NULL DEFAULT '',
     PRIMARY KEY (ID),
-    FOREIGN KEY (question) REFERENCES questions (questionNearID)
-    );
+    FOREIGN KEY (question) REFERENCES questions (questionNearID)	
+);
 
 CREATE TABLE theme (
-	ID				INT UNSIGNED	NOT NULL AUTO_INCREMENT,
-    label			VARCHAR(50)		NOT NULL DEFAULT '',
+	ID				INT UNSIGNED	NOT NULL AUTO_INCREMENT,		#Table of the different themes. Each theme can have a parent.
+    label			VARCHAR(50)		NOT NULL DEFAULT '',			#So we have a tree
     parent			INT UNSIGNED	DEFAULT 0,
     PRIMARY KEY (ID)
 );
 
+CREATE TABLE difficulty(
+	ID				INT UNSIGNED	NOT NULL AUTO_INCREMENT,
+    difficulty		VARCHAR(50)		NOT NULL DEFAULT '',
+    PRIMARY KEY (ID)
+);
+
 CREATE TABLE users (
-	userID			INT UNSIGNED	NOT NULL AUTO_INCREMENT,
+	ID				INT UNSIGNED	NOT NULL AUTO_INCREMENT,
     telegramID		INT UNSIGNED	NOT NULL DEFAULT 0,
-    PRIMARY KEY (userID)
-    );
+    PRIMARY KEY (ID)
+);
+
 ALTER TABLE users ADD UNIQUE (telegramID);
 
 CREATE TABLE knowledge (
-	ID				INT UNSIGNED	NOT NULL AUTO_INCREMENT,
-	user			INT UNSIGNED	NOT NULL DEFAULT '',
-    knowledge		VARCHAR(15)		NOT NULL DEFAULT '',
+	ID				INT UNSIGNED	NOT NULL AUTO_INCREMENT,		#Storage of the knowledge of each user in a specific theme
+	user			INT UNSIGNED	NOT NULL DEFAULT 0,
+    difficulty		INT UNSIGNED	NOT NULL DEFAULT 0,
     theme			INT UNSIGNED	NOT NULL DEFAULT 0,
     PRIMARY KEY(ID),
-    FOREIGN KEY (user) REFERENCES users (userID)
+    FOREIGN KEY (user) REFERENCES users (ID),
+    FOREIGN KEY (difficulty) REFERENCES difficulty (ID),
+    FOREIGN KEY (theme) REFERENCES theme(ID)
 );
 
 CREATE TABLE resources(
-	resourceID		INT UNSIGNED	NOT NULL AUTO_INCREMENT,
+	ID				INT UNSIGNED	NOT NULL AUTO_INCREMENT,		#Here, we will have the course, articles, etc. that someone will need acording to his/her knowledge about theme
     resourceSite	VARCHAR(500)	NOT NULL DEFAULT '',
     theme			INT UNSIGNED	NOT NULL DEFAULT 0,
-    knowledge		VARCHAR(50)		NOT NULL DEFAULT '',
-    PRIMARY KEY(resourceID)
-    );
+    difficulty		INT UNSIGNED	NOT NULL DEFAULT 0,
+    PRIMARY KEY(ID),
+    FOREIGN KEY(theme) REFERENCES theme (ID),
+    FOREIGN KEY(difficulty) REFERENCES difficulty (ID)
+);
     
