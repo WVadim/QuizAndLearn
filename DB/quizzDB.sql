d4a5ad04d6b6a4db670e221b691729f7a14ea3ae
Vadim Kirilin <vadim.kirilin@imdea.org>
2017-04-04 13:46:25 +0200

    fixed ubuntu bugs on DB + reduction of questions

--- DB/quizzDB.sql
+++ DB/quizzDB.sql
@@ -1,66 +1,70 @@
-CREATE DATABASE quizz;
+DROP DATABASE IF EXISTS quizDB;
 
-CREATE TABLE questions ( 
-	questionID		INT UNSIGNED 	NOT NULL AUTO_INCREMENT,
-    questionNearID  INT UNSIGNED    NOT NULL DEFAULT 0,				#Questions with the same meaning or close to each other, will have the same questionNearID
-	question		VARCHAR(1000)	NOT NULL DEFAULT '',			
-	answer			VARCHAR(500)	NOT NULL DEFAULT '',
-    difficulty		INT UNSIGNED	NOT NULL DEFAULT 0,				
-	theme			INT UNSIGNED	NOT NULL DEFAULT 0,				
-    questionMaker	INT UNSIGNED	NOT NULL DEFAULT '',			
+CREATE DATABASE quizDB;
+
+USE quizDB;
+
+CREATE TABLE questions (
+	QuestionID			INT UNSIGNED 	NOT NULL AUTO_INCREMENT,
 	PRIMARY KEY (questionID),
-    FOREIGN KEY (questionMaker) REFERENCES users (ID),
-    FOREIGN KEY (theme) REFERENCES theme (ID),
-    FOREIGN KEY (difficulty) REFERENCES difficulty (ID)
+	QuestionText			TEXT		NOT NULL,			
+	Difficulty			INT UNSIGNED	NOT NULL DEFAULT 0,
+	Theme				INT UNSIGNED	NOT NULL DEFAULT 0,						
+	QuestionMaker			INT UNSIGNED	NOT NULL DEFAULT 0		
 );
-    
+
 CREATE TABLE answers (
-	ID				INT UNSIGNED	NOT NULL AUTO_INCREMENT,		#Storage of most frequent answer (taken as the correct one)
-    question		INT UNSIGNED	NOT NULL DEFAULT 0,
-    answer			VARCHAR(500)	NOT NULL DEFAULT '',
-    PRIMARY KEY (ID),
-    FOREIGN KEY (question) REFERENCES questions (questionNearID)	
+	ID				INT UNSIGNED	NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (ID),		
+	#Storage of most frequent answer (taken as the correct one)
+	QuestionID			INT UNSIGNED	NOT NULL DEFAULT 0,
+	AnswerText			TEXT		NOT NULL
 );
 
 CREATE TABLE theme (
-	ID				INT UNSIGNED	NOT NULL AUTO_INCREMENT,		#Table of the different themes. Each theme can have a parent.
-    label			VARCHAR(50)		NOT NULL DEFAULT '',			#So we have a tree
-    parent			INT UNSIGNED	DEFAULT 0,
-    PRIMARY KEY (ID)
+	ID				INT UNSIGNED	NOT NULL AUTO_INCREMENT,	#Table of the different themes. Each theme can have a parent.
+	PRIMARY KEY (ID),
+	Label				TINYTEXT	NOT NULL,			#So we have a tree
+	Parent				INT UNSIGNED	DEFAULT 0
 );
 
 CREATE TABLE difficulty(
 	ID				INT UNSIGNED	NOT NULL AUTO_INCREMENT,
-    difficulty		VARCHAR(50)		NOT NULL DEFAULT '',
-    PRIMARY KEY (ID)
+	PRIMARY KEY (ID),
+	Difficulty			TINYTEXT	NOT NULL
 );
 
 CREATE TABLE users (
 	ID				INT UNSIGNED	NOT NULL AUTO_INCREMENT,
-    telegramID		INT UNSIGNED	NOT NULL DEFAULT 0,
-    PRIMARY KEY (ID)
+	PRIMARY KEY (ID),
+	TelegramID			INT UNSIGNED	NOT NULL DEFAULT 0
 );
-
-ALTER TABLE users ADD UNIQUE (telegramID);
 
 CREATE TABLE knowledge (
 	ID				INT UNSIGNED	NOT NULL AUTO_INCREMENT,		#Storage of the knowledge of each user in a specific theme
-	user			INT UNSIGNED	NOT NULL DEFAULT 0,
-    difficulty		INT UNSIGNED	NOT NULL DEFAULT 0,
-    theme			INT UNSIGNED	NOT NULL DEFAULT 0,
-    PRIMARY KEY(ID),
-    FOREIGN KEY (user) REFERENCES users (ID),
-    FOREIGN KEY (difficulty) REFERENCES difficulty (ID),
-    FOREIGN KEY (theme) REFERENCES theme(ID)
+	PRIMARY KEY(ID),
+	UserID				INT UNSIGNED	NOT NULL DEFAULT 0,
+	Difficulty			INT UNSIGNED	NOT NULL DEFAULT 0,
+	Theme				INT UNSIGNED	NOT NULL DEFAULT 0
 );
 
-CREATE TABLE resources(
-	ID				INT UNSIGNED	NOT NULL AUTO_INCREMENT,		#Here, we will have the course, articles, etc. that someone will need acording to his/her knowledge about theme
-    resourceSite	VARCHAR(500)	NOT NULL DEFAULT '',
-    theme			INT UNSIGNED	NOT NULL DEFAULT 0,
-    difficulty		INT UNSIGNED	NOT NULL DEFAULT 0,
-    PRIMARY KEY(ID),
-    FOREIGN KEY(theme) REFERENCES theme (ID),
-    FOREIGN KEY(difficulty) REFERENCES difficulty (ID)
+CREATE TABLE resources (
+	ID				INT UNSIGNED	NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY(ID),		
+#Here, we will have the course, articles, etc. that someone will need acording to his/her knowledge about theme
+	ResourceSite			TEXT		NOT NULL,
+	Theme				INT UNSIGNED	NOT NULL DEFAULT 0,
+	Difficulty			INT UNSIGNED	NOT NULL DEFAULT 0
 );
-    
+
+ALTER TABLE questions 	ADD CONSTRAINT DifficultyMarker FOREIGN KEY (Difficulty) 	REFERENCES difficulty(ID) 		ON UPDATE CASCADE;
+ALTER TABLE questions 	ADD CONSTRAINT ThemeMarker 	FOREIGN KEY (Theme) 		REFERENCES theme(ID) 			ON UPDATE CASCADE;
+ALTER TABLE questions 	ADD CONSTRAINT QuestionCreator 	FOREIGN KEY (QuestionMaker) 	REFERENCES users(ID) 			ON UPDATE CASCADE;
+ALTER TABLE answers	ADD CONSTRAINT QuestionRefer 	FOREIGN KEY (QuestionID) 	REFERENCES questions(QuestionID) 	ON UPDATE CASCADE;
+ALTER TABLE users 	ADD 				UNIQUE (TelegramID);
+ALTER TABLE knowledge	ADD CONSTRAINT KnowledgeUser 	FOREIGN KEY (UserID)	 	REFERENCES users(ID)		 	ON UPDATE CASCADE;
+ALTER TABLE knowledge	ADD CONSTRAINT KnowledgeDiff	FOREIGN KEY (Difficulty)	REFERENCES difficulty(ID)		ON UPDATE CASCADE;
+ALTER TABLE knowledge	ADD CONSTRAINT KnowledgeTheme 	FOREIGN KEY (Theme)	 	REFERENCES theme(ID)		 	ON UPDATE CASCADE;
+ALTER TABLE resources	ADD CONSTRAINT ResourceTheme 	FOREIGN KEY (Theme)	 	REFERENCES theme(ID)		 	ON UPDATE CASCADE;
+ALTER TABLE resources	ADD CONSTRAINT ResourceDiff	FOREIGN KEY (Difficulty)	REFERENCES difficulty(ID)		ON UPDATE CASCADE;
+
