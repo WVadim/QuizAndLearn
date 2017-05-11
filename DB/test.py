fa0c9d5d0835bf77b678f93b382d1c33ac9e74c0
Javi <javipe19@gmail.com>
2017-05-11 18:23:29 +0200

    fucking shit test

--- DB/test.py
+++ DB/test.py
@@ -1,8 +1,7 @@
 from DBInterface import *
 
 # Create objects
-# Check API for creation, almost all parameters unnecessary
-
+#Check API for creation, almost all parameters unnecessary
 print 'Users :'
 print DBInterace.CreateUser(1, u'Vasya', u'Pupkin').firstname
 print DBInterace.CreateUser(2, u'Vadim', u'Kirilin').firstname
@@ -18,13 +17,142 @@ biology_mammals = DBInterace.s_CreateTheme(label='Mammals', parent=biology_theme
 print 'Theme Biology-Mammals created, label :', biology_mammals.label
 geography_mammals = DBInterace.s_CreateTheme(label='Mammals', parent=geography_theme.id)
 print 'Theme Geography-Mammals created, label :', geography_mammals.label
-
+geography_mammals = DBInterace.s_CreateTheme(label='Arts',  parent=0)
+geography_mammals = DBInterace.s_CreateTheme(label='Enterteinment',  parent=0)
+geography_mammals = DBInterace.s_CreateTheme(label='History',  parent=0)
+geography_mammals = DBInterace.s_CreateTheme(label='Science & Technology',  parent=0)
+geography_mammals = DBInterace.s_CreateTheme(label='Sports',  parent=0)
 
 # Use s_CreateDifficulty instead of CreateDifficulty, s_ - safe method
 # It will allow you to avoid dpulicates in database, such as two difficulties with tag "Hard" but with different ids
 print 'Difficulties :'
-print DBInterace.s_CreateDifficulty(difficulty='Hard').difficulty
 print DBInterace.s_CreateDifficulty(difficulty='Easy').difficulty
+print DBInterace.s_CreateDifficulty(difficulty='Medium').difficulty
+print DBInterace.s_CreateDifficulty(difficulty='Hard').difficulty
+
+
+question1= DBInterace.s_CreateQuestion(text="What is the location of London's National Gallery?",difficulty=3,theme=5,creator=1)
+question2= DBInterace.s_CreateQuestion(text="What nationality was Edvard Munch, who produced the famous painting The Scream in 1893?",difficulty=2,theme=5,creator=1)
+question3= DBInterace.s_CreateQuestion(text="Pablo Picasso was born in 1881 in which Spanish city?",difficulty=2,theme=5,creator=1)
+question4= DBInterace.s_CreateQuestion(text="Which Dutch artist painted the Laughing Cavalier in 1624?",difficulty=1,theme=5,creator=1)
+question5= DBInterace.s_CreateQuestion(text="Which English artist painted Rain, Steam and Speed - The Great Western Railway in 1844?",difficulty=1,theme=5,creator=1)
+question6= DBInterace.s_CreateQuestion(text="What is the colloquial name for the 1871 painting Arrangement in Grey and Black: The Artist's Mother?",difficulty=1,theme=5,creator=1)
+question7= DBInterace.s_CreateQuestion(text="Between 1887 and 1888 Vincent van Gogh produced a series of still life paintings depicting which flowers?",difficulty=2,theme=5,creator=1)
+question8= DBInterace.s_CreateQuestion(text="The English artist George Stubbs, who died in 1806, is best remembered for his paintings of what?",difficulty=2,theme=5,creator=1)
+question9= DBInterace.s_CreateQuestion(text="Who created the modern sculpture Angel of the North, that overlooks the A1 road and the East Coast main line in Gateshead?",difficulty=1,theme=5,creator=1)
+question10= DBInterace.s_CreateQuestion(text="Hans Holbein the Younger was criticized by Henry VIII for painting a flattering portrait of whom?",difficulty=2,theme=5,creator=1)
+question11= DBInterace.s_CreateQuestion(text="What spoiled Becky and Steve's wedding reception in Coronation Street last week?",difficulty=2,theme=6,creator=1)
+question12= DBInterace.s_CreateQuestion(text="The BBC announced last week that which former presenter has set up his own version of the Blue Peter garden in Edinburgh?",difficulty=1,theme=6,creator=1)
+question13= DBInterace.s_CreateQuestion(text="Who plays the title role in the film The Time Traveler's Wife, released last Friday?",difficulty=3,theme=6,creator=1)
+question14= DBInterace.s_CreateQuestion(text="Which celebrity signed a 1.5 million deal last week to publish a cookbook?",difficulty=2,theme=6,creator=1)
+question15= DBInterace.s_CreateQuestion(text="What is the title of Beyonce's latest single, that reached No 5 in the charts last week?",difficulty=3,theme=6,creator=1)
+question16= DBInterace.s_CreateQuestion(text="The new Quentin Tarantino film Inglourious Basterds, due for release on Friday, is set during which war?",difficulty=2,theme=6,creator=1)
+question17= DBInterace.s_CreateQuestion(text="Which rock band last week released a 20th anniversary edition of their debut album?",difficulty=2,theme=6,creator=1)
+question18= DBInterace.s_CreateQuestion(text="The BBC announced last week that which Radio 1 DJ is to leave the station due to a scheduling shake-up?",difficulty=2,theme=6,creator=1)
+question19= DBInterace.s_CreateQuestion(text="Which EastEnders character last week followed his lady- love Brenda as she emigrated to Madeira?",difficulty=1,theme=6,creator=1)
+question20= DBInterace.s_CreateQuestion(text="What is the real name of the Big Brother housemate Halfwit, who was evicted from the show last week?",difficulty=3,theme=6,creator=1)
+question21= DBInterace.s_CreateQuestion(text="Which German city lies farthest south - Dortmund, Duisburg, Essen or Leipzig?",difficulty=2,theme=2,creator=1)
+question22= DBInterace.s_CreateQuestion(text="Of Greece's 2000 islands, how many are inhabited?",difficulty=3,theme=2,creator=1)
+question23= DBInterace.s_CreateQuestion(text="What is the administrative centre of County Kildare in Ireland?",difficulty=1,theme=2,creator=1)
+question24= DBInterace.s_CreateQuestion(text="Near which Italian city is Frascati wine produced?",difficulty=1,theme=2,creator=1)
+question25= DBInterace.s_CreateQuestion(text="In which country is Lake Balaton?",difficulty=1,theme=2,creator=1)
+question26= DBInterace.s_CreateQuestion(text="Which Spanish city contains the Alcazar, whose construction began in 1531?",difficulty=3,theme=2,creator=1)
+question27= DBInterace.s_CreateQuestion(text="Famous for its local cheese, near which city is Roquefort located?",difficulty=3,theme=2,creator=1)
+question28= DBInterace.s_CreateQuestion(text="Of which sport is \"pesapallo\" a local Finnish variation?",difficulty=3,theme=2,creator=1)
+question29= DBInterace.s_CreateQuestion(text="Which European country was ruled until 1985 by Enver Hoxha?",difficulty=2,theme=2,creator=1)
+question30= DBInterace.s_CreateQuestion(text="Which has the largest population - Bremen, Lille, Ljubljana or Reykjavik?",difficulty=3,theme=2,creator=1)
+question31= DBInterace.s_CreateQuestion(text="Approximately how many men did the British lose at the Battle of Jutland in 1916?",difficulty=1,theme=7,creator=1)
+question32= DBInterace.s_CreateQuestion(text="In which year did Chaucer complete \"The Canterbury Tales\"?",difficulty=3,theme=7,creator=1)
+question33= DBInterace.s_CreateQuestion(text="Of which independent state did Kwame Nkrumah become the first Prime Minister?",difficulty=2,theme=7,creator=1)
+question34= DBInterace.s_CreateQuestion(text="In which year was Marie-Antoinette executed?",difficulty=1,theme=7,creator=1)
+question35= DBInterace.s_CreateQuestion(text="Where did the Venetian merchant and adventurer Marco Polo die?",difficulty=2,theme=7,creator=1)
+question36= DBInterace.s_CreateQuestion(text="In which year did the Middle East's \"Six Day War\" take place?",difficulty=1,theme=7,creator=1)
+question37= DBInterace.s_CreateQuestion(text="Against which country did Russia wage war in 1904-05?",difficulty=3,theme=7,creator=1)
+question38= DBInterace.s_CreateQuestion(text="Founded in 1746, which is the fourth-oldest university in the USA?",difficulty=2,theme=7,creator=1)
+question39= DBInterace.s_CreateQuestion(text="During the reign of Gustav IV, which country lost the territory of Finland in 1808?",difficulty=2,theme=7,creator=1)
+question40= DBInterace.s_CreateQuestion(text="In which country did the South American freedom fighter Simon Bolivar die in 1830?",difficulty=1,theme=7,creator=1)
+question41= DBInterace.s_CreateQuestion(text="Who co-founded the Apple Corporation with Steve Wozniak in 1976?",difficulty=3,theme=8,creator=1)
+question42= DBInterace.s_CreateQuestion(text="What do the initials stand for in the name of the US television network PBS?",difficulty=1,theme=8,creator=1)
+question43= DBInterace.s_CreateQuestion(text="In September 1961 Border Television began transmitting from where?",difficulty=1,theme=8,creator=1)
+question44= DBInterace.s_CreateQuestion(text="In 1792 the French inventor Claude Chappe built the world's first practical semaphore system between Paris and which other city?",difficulty=3,theme=8,creator=1)
+question45= DBInterace.s_CreateQuestion(text="In which US state are the headquarters of the Microsoft Corporation?",difficulty=2,theme=8,creator=1)
+question46= DBInterace.s_CreateQuestion(text="When ITV began broadcasting in 1955, what was the first product to be advertised?",difficulty=1,theme=8,creator=1)
+question47= DBInterace.s_CreateQuestion(text="Which comedian made the UK's first mobile phone call on I January 1985?",difficulty=1,theme=8,creator=1)
+question48= DBInterace.s_CreateQuestion(text="Which piece of computer hardware was invented by Douglas Engelbart in the USA in 1964?",difficulty=2,theme=8,creator=1)
+question49= DBInterace.s_CreateQuestion(text="What stopped BBC from broadcasting on its intended launch date of 20 April 1964?",difficulty=3,theme=8,creator=1)
+question50= DBInterace.s_CreateQuestion(text="Which offshore radio station began broadcasting on Easter Sunday 1965?",difficulty=2,theme=8,creator=1)
+question51= DBInterace.s_CreateQuestion(text="Former England football manager Bobby Robson, who died last week, was born in which County Durham mining village?",difficulty=3,theme=9,creator=1)
+question52= DBInterace.s_CreateQuestion(text="Edgbaston, the venue for this year's third Ashes test, is the home of which cricketing county?",difficulty=2,theme=9,creator=1)
+question53= DBInterace.s_CreateQuestion(text="Which US TV network is to broadcast live Premier League games this coming season, following the collapse of Setanta?",difficulty=3,theme=9,creator=1)
+question54= DBInterace.s_CreateQuestion(text="Which golf course staged this year's British Women's Open?",difficulty=1,theme=9,creator=1)
+question55= DBInterace.s_CreateQuestion(text="Spurs beat Hull City last week in the final of the Asia Cup, in which city?",difficulty=2,theme=9,creator=1)
+question56= DBInterace.s_CreateQuestion(text="What is the name of the contract recently signed by the FIA and the Formula One Teams' Association?",difficulty=2,theme=9,creator=1)
+question57= DBInterace.s_CreateQuestion(text="Which 2009 Tour de France stage winner was suspended last week after testing positive for the blood-booster EPO?",difficulty=3,theme=9,creator=1)
+question58= DBInterace.s_CreateQuestion(text="The 2013 Rugby League World Cup, recently awarded to Britain, will be contested by how many teams?",difficulty=1,theme=9,creator=1)
+question59= DBInterace.s_CreateQuestion(text="Serena Williams was knocked out of the Bank of the West tennis tournament in Stanford, California last week by Samantha Stosur",difficulty=3,theme=9,creator=1)
+question60= DBInterace.s_CreateQuestion(text="The Goodwood Cup was won last Thursday by Schiaparelli. Who was the winning jockey?",difficulty=3,theme=9,creator=1)
+
+
+answer1 =DBInterace.s_CreateAnswer(1,"Trafalgar Square")
+answer2 =DBInterace.s_CreateAnswer(2,"Norwegian")
+answer3 =DBInterace.s_CreateAnswer(3,"Malaga")
+answer4 =DBInterace.s_CreateAnswer(4,"Frans Hals")
+answer5 =DBInterace.s_CreateAnswer(5,"J M W Turner")
+answer6 =DBInterace.s_CreateAnswer(6,"Whistler's Mother")
+answer7 =DBInterace.s_CreateAnswer(7,"Sunflowers")
+answer8 =DBInterace.s_CreateAnswer(8,"Horses")
+answer9 =DBInterace.s_CreateAnswer(9,"Antony Gormley")
+answer10 =DBInterace.s_CreateAnswer(10,"Anne of Cleves")
+answer11 =DBInterace.s_CreateAnswer(11,"A drugs raid")
+answer12 =DBInterace.s_CreateAnswer(12,"Peter Duncan")
+answer13 =DBInterace.s_CreateAnswer(13,"Rachel McAdams")
+answer14 =DBInterace.s_CreateAnswer(14,"Peter Andre")
+answer15 =DBInterace.s_CreateAnswer(15,"Sweet Dreams")
+answer16 =DBInterace.s_CreateAnswer(16,"World War II")
+answer17 =DBInterace.s_CreateAnswer(17,"Stone Roses")
+answer18 =DBInterace.s_CreateAnswer(18,"Steve Lamacq")
+answer19 =DBInterace.s_CreateAnswer(19,"Charlie Slater")
+answer20 =DBInterace.s_CreateAnswer(20,"Freddie Fisher")
+answer21 =DBInterace.s_CreateAnswer(21,"Leipzig")
+answer22 =DBInterace.s_CreateAnswer(22,"170")
+answer23 =DBInterace.s_CreateAnswer(23,"Naas")
+answer24 =DBInterace.s_CreateAnswer(24,"Rome")
+answer25 =DBInterace.s_CreateAnswer(25,"Hungary")
+answer26 =DBInterace.s_CreateAnswer(26,"Toledo")
+answer27 =DBInterace.s_CreateAnswer(27,"Toulouse")
+answer28 =DBInterace.s_CreateAnswer(28,"Baseball")
+answer29 =DBInterace.s_CreateAnswer(29,"Albania")
+answer30 =DBInterace.s_CreateAnswer(30,"Bremen")
+answer31 =DBInterace.s_CreateAnswer(31,"6300")
+answer32 =DBInterace.s_CreateAnswer(32,"Unfinished at the time of his death")
+answer33 =DBInterace.s_CreateAnswer(33,"Ghana")
+answer34 =DBInterace.s_CreateAnswer(34,"1793")
+answer35 =DBInterace.s_CreateAnswer(35,"Venice")
+answer36 =DBInterace.s_CreateAnswer(36,"1967")
+answer37 =DBInterace.s_CreateAnswer(37,"Japan")
+answer38 =DBInterace.s_CreateAnswer(38,"Princeton")
+answer39 =DBInterace.s_CreateAnswer(39,"Sweden")
+answer40 =DBInterace.s_CreateAnswer(40,"Colombia")
+answer41 =DBInterace.s_CreateAnswer(41,"Steve Jobs")
+answer42 =DBInterace.s_CreateAnswer(42,"Public Broadcasting Service")
+answer43 =DBInterace.s_CreateAnswer(43,"Carlisle")
+answer44 =DBInterace.s_CreateAnswer(44,"Lille")
+answer45 =DBInterace.s_CreateAnswer(45,"Washington")
+answer46 =DBInterace.s_CreateAnswer(46,"Toothpaste")
+answer47 =DBInterace.s_CreateAnswer(47,"Venice")
+answer48 =DBInterace.s_CreateAnswer(48,"The mouse")
+answer49 =DBInterace.s_CreateAnswer(49,"A power cut")
+answer50 =DBInterace.s_CreateAnswer(50,"Radio Caroline")
+answer51 =DBInterace.s_CreateAnswer(51,"Sacriston ")
+answer52 =DBInterace.s_CreateAnswer(52,"Warwickshire")
+answer53 =DBInterace.s_CreateAnswer(53,"ESPN")
+answer54 =DBInterace.s_CreateAnswer(54,"Royal Lytham")
+answer55 =DBInterace.s_CreateAnswer(55,"Beijing")
+answer56 =DBInterace.s_CreateAnswer(56,"Concorde Agreement")
+answer57 =DBInterace.s_CreateAnswer(57,"Mikel Astarloza")
+answer58 =DBInterace.s_CreateAnswer(58,"12")
+answer59 =DBInterace.s_CreateAnswer(59,"What nationality is Stosur?")
+answer60 =DBInterace.s_CreateAnswer(60,"Frankie Dettori")
 
 knowledge = DBInterace.GetKnowledge(person=1)
 print 'Person knowledge :', knowledge
@@ -158,3 +286,6 @@ else:
     answers = sorted(answers, key=lambda x : x.frequency, reverse=True)
     best_answer = answers[0]
 
+
+
+DBInterace.UpdateAnswer(answer1.id, answer1.frequency + 1)
